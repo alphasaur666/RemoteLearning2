84850fba32fa97317cc85f0aeefea2bcbc16cf7e
Roșu Răzvan-Virgil <celtics.razvan@gmail.com>
2020-04-01 19:43:17 +0300

    Added the first week homework.

---
+++ HelloWrodl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWrodl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello wrodl!");
+        }
+    }
+}
