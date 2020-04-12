8ca30837f76bd79547c3bc3af674aa0645b56350
Roșu Răzvan-Virgil <celtics.razvan@gmail.com>
2020-04-12 16:07:16 +0300

    Uploaded first version of Reflection homework

---
+++ Week3/DBAccess/Attributes/KeyAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DBAccess.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public sealed class KeyAttribute : Attribute
+    {
+        public string Name { get; private set; }
+
+        public KeyAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Week3/DBAccess/Attributes/TableNameAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DBAccess.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public sealed class TableNameAttribute : Attribute
+    {
+        public string Name { get; private set; }
+
+        public TableNameAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
