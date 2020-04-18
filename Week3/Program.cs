ff0d1da0ffd5b8bd4b116df7c21266057ccf171b
Roșu Răzvan-Virgil <celtics.razvan@gmail.com>
2020-04-18 16:43:50 +0300

    Added some untested code that I wrote, implemented SHA hash, etc

---
+++ Week3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TaskHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mover mover = new Mover();
+            try
+            {
+
+            }
+            catch(Exception e)
+            {
+                
+            }
+        }
+    }
+}
---
+++ Week3/Targeter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskHomework
+{
+    class Targeter
+    {
+        public string fileName;
+        public string targetPath;
+        Task<string> characterReader = ReadCharacters(@".\CallOfTheWild.txt");
+        //string readerResult = characterReader.Result;
+
+
+        private static async Task<string> ReadCharacters(string fn)
+        {
+            string text;
+            using(StreamReader streamReader = new StreamReader(fn))
+            {
+                text = await streamReader.ReadToEndAsync();
+            }
+            return text;
+        }
+
+
+        private string computeSHA(string rawData)
+        {
+            using(SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+                StringBuilder stringBuilder = new StringBuilder();
+                for(int i = 0; i < bytes.Length; i++)
+                {
+                    stringBuilder.Append(bytes[i].ToString("x2"));
+                }
+                return stringBuilder.ToString();
+
+            }
+        }
+    }
+
+
+}
